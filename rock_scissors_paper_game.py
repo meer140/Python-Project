eefbe908d4481885070e25ae8fcf35bee9e1f90b
meer140 <mirmustafa2005ali@gmail.com>
2025-04-17 08:44:15 +0500

    improve the code

--- rock_scissors_paper_game.py
+++ rock_scissors_paper_game.py
@@ -1,20 +1,24 @@
 import random
 
-list = ['r','s','p']
+Rock = 'r'
+Scissors = 's'
+Paper = 'p'
+
 dic = {'r' : "⚫", 's' : "✂️", 'p' : "📄"}
+choices = tuple(dic.keys())
 
 def get_userchoice():
     while True:
         user_choice =  input("Choose one rock, paper or scissors(r/p/s): ").lower()
-        if user_choice in list:
+        if user_choice in choices:
             return user_choice
         else:
             print("Invalid Input")
         
 def result(user_choice, comp_choice): 
-     if ((user_choice == 'r' and  comp_choice == 'scissors') or
-            (user_choice == 'p' and comp_choice == 'rock') or
-            (user_choice == 's' and comp_choice == 'paper')) :
+     if ((user_choice == Rock and  comp_choice == Scissors) or
+            (user_choice == Paper and comp_choice == Rock) or
+            (user_choice == Scissors and comp_choice == Paper)) :
             print("You win the game!")
      elif user_choice == comp_choice:
             print("Tie")    
@@ -30,7 +34,7 @@ def main():
         user_choice = get_userchoice()
         
         print("You choose ",dic[user_choice])
-        comp_choice = random.choice(list)
+        comp_choice = random.choice(choices)
         print("Computer choose ",dic[comp_choice])
        
         result(user_choice, comp_choice)  
